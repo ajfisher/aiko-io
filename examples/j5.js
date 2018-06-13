96323de43d82c84ce3061e0330739973db5b0391
ajfisher <ajfisher.td@gmail.com>
2018-06-13 20:30:12 +1000

    Linter nits

--- examples/j5.js
+++ examples/j5.js
@@ -3,21 +3,20 @@ import five from 'johnny-five';
 import AikoIO from '../src/index.js';
 
 const board = five.Board({
-    io: new AikoIO({
-        platform: 'AIKO32',
-        transport: {
-            type: 'mqtt',
-            host: 'mqtt://iot.local',
-            topic: 'aiko_esp/3914cc/0',
-        },
-    }),
+  io: new AikoIO({
+    platform: 'AIKO32',
+    transport: {
+      type: 'mqtt',
+      // host: 'mqtt://iot.local',
+      host: 'mqtt://iot.eclipse.org',
+      topic: 'aiko_esp/4e8770/0'
+    }
+  })
 });
 
 board.on('ready', () => {
+  console.log('ready');
 
-    console.log("ready");
-
-    let led = new five.Led({pin: 22});
-    led.blink(5000);
-
+  const led = new five.Led({pin: 22});
+  led.blink(2000);
 });
